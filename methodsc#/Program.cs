7cecae9a0b292cd3262e466a0c21866440233933
danishbukhari4599 <danishbukhari4599@gmail.com>
2024-03-16 10:04:12 +0500

    Add project files.

---
+++ methodsc#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace methodsc
+{
+    class Program
+    {
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " is " + age);
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod("Liam", 5);
+            MyMethod("Jenny", 8);
+            MyMethod("Anja", 31);
+        }
+    }
+}
